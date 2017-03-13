2530aebf39d523e522755030fc48482b33bca734
Firaenix <Nick.Carton@gmail.com>
2017-03-13 21:22:40 +1100

    dumping new data

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var server = express();
 server.use('/', express.static(__dirname + '/'));
+// server.get('/', (res, req) => {
+//   console.log(res);
+// });
+server.post('/pwned', (req, res) => {
+  console.log(req.body);
+});
 server.listen(80);
 
 console.log('Server running on port 80');
